e518ce4894d4f0fe7fdb1cc275905822f9a7feed
DennisAlves <dennis.alves@outlook.com>
2020-02-07 11:28:19 -0300

    exercicios de interpretação de codigo ex 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,5 +3,13 @@
 pela const cotacao atraves de um prompt a const meuDinheiro recebe oresultado função om o valor 100 e depois é
 impresso no console log
 
+2. a função investeDinheiro recebe duas variaveis que são a tipoDeInvestimento e valor, dentro da função é criado uma
+variavel let valorAposInvestimento que será usado para receber o resultado das operações realizadas dentro do switch
+esse switch recebe o tipoDeInvestimento para definer o case e realizar a operação e retornar valorAposInvestimento,
+a const novoMontante recebe o retorno da função investeDinheiro com os paremetros ações e 150 que no caso ira retornar
+valor(150)*1.1
+a const segundoMontante recebe o retorno da função investeDinheiro com os paremetros Tesouro Direto e 200 que nocaso ira
+retornar a menssagem de alerta TIPO DE INVESTIMENTO INFORMADO INCORRETO ambas constantes serão impressas pelo console log
+
 */
 

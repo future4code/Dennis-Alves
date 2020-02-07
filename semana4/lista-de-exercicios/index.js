3eb23094e7ee542b8763a98d8c9df16de875b56d
DennisAlves <dennis.alves@outlook.com>
2020-02-07 11:27:43 -0300

    exercicios de interpretação de codigo ex 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,7 @@
+/*exercicios de interpretação de codigo
+1. a função conversorDeMoeda recebe um valor par ser convertido em dolar, a cotação do dolar é recebida
+pela const cotacao atraves de um prompt a const meuDinheiro recebe oresultado função om o valor 100 e depois é
+impresso no console log
+
+*/
+

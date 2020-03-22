723bf3597538ff4b3617338a4d264aea602febbb
DennisAlves <dennis.alves@outlook.com>
2020-03-22 20:45:59 -0300

    entrega da tarefa 'astro-match'

---
+++ semana8/astro-match/build/precache-manifest.364cd0d24848406e1bc3382facf496c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29c38011051838bba6439a3b7a2f138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7788d0f87c91090da634",
+    "url": "/static/js/2.9afeef9c.chunk.js"
+  },
+  {
+    "revision": "89ac0dd599e7db30d67a",
+    "url": "/static/js/main.f2378e8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
